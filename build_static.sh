8f8a8ed4ab4f913f0cc6a0780221d1306e8ea66b
Ovidiu Ionescu <ovidiu@ionescu.net>
2020-06-08 01:23:28 +0200

    chore: tag gets picked from version file

--- build_static.sh
+++ build_static.sh
@@ -2,3 +2,5 @@ cargo build --release --target x86_64-unknown-linux-musl
 
 strip target/x86_64-unknown-linux-musl/release/organizator-server
 
+git tag -a v$(head src/version.txt) -F src/version.txt
+
